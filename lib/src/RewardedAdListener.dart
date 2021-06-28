e87230772d8a86b382beda30a196bb8237faf522
kkawai <kkawai2@gmail.com>
2021-06-28 00:13:17 -0700

    updated comment

--- lib/src/RewardedAdListener.dart
+++ lib/src/RewardedAdListener.dart
@@ -1,13 +1,20 @@
 class RewardedAdListener {
+
+  // The rewarded ad has been loaded and is ready to be shown.
   void onRewardedVideoLoaded(String? placement) {}
 
+  // The rewarded ad failed to load.
   void onRewardedVideoFailed(String? placement, String errorMessage) {}
 
+  // The rewarded ad has shown on the screen.
   void onRewardedVideoShown(String? placement) {}
 
+  // The rewarded ad encountered an error when it was about to show on screen.
   void onRewardedVideoShownError(String? placement, String errorMessage) {}
 
+  // The rewarded ad was dismissed.
   void onRewardedVideoDismissed(String? placement) {}
 
+  // The rewarded ad has completed and the user can be rewarded.
   void onRewardedVideoCompleted(String? placement) {}
 }
