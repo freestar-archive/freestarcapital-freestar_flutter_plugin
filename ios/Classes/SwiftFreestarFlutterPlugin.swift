29851f18d72fa9b07220dfaeba45868bff8a62f1
Lev Trubov <lev.trubov@freestar.io>
2021-07-08 12:38:47 -0700

    ios app running and loading ads -- no linking for display yet

--- ios/Classes/SwiftFreestarFlutterPlugin.swift
+++ ios/Classes/SwiftFreestarFlutterPlugin.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 
+import FreestarAds
+
 public class SwiftFreestarFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "freestar_flutter_plugin", binaryMessenger: registrar.messenger())
@@ -9,6 +11,23 @@ public class SwiftFreestarFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    if call.method == "init" {
+        guard let apiKey = firstArg(call) else {
+            print("No API Key Passed to init")
+            return
+        }
+        Freestar.initWithAdUnitID(apiKey)
+    }
+    
     result("iOS " + UIDevice.current.systemVersion)
   }
+    
+    private func firstArg(_ call: FlutterMethodCall) -> String? {
+        guard let args = call.arguments else { return nil }
+        if let fa = args as? String {
+            return fa
+        }
+        
+        return nil
+    }
 }
